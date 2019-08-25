1230bdc537430414f09130c4e08374f22389fa48
cpzhang <297191409@qq.com>
2019-08-25 08:37:33 +0800

    nginx

--- python/hello.py
+++ python/hello.py
@@ -1,2 +1,10 @@
+﻿print("hello world")
+import pyodbc 
+cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};Server=59.111.61.113,38065;Database=ds;UID=sa;PWD=Love2016;")
 
-print("hello world")
+
+cursor = cnxn.cursor()
+cursor.execute('SELECT top 10 ActorName FROM GameActor')
+
+for row in cursor:
+    print('row = %r' % (row,))
