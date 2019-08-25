77ee00c50c9822e733e22f6cb6fb62553727ce03
cpzhang <297191409@qq.com>
2019-08-25 09:12:20 +0800

    python sql server connect

--- python/hello.py
+++ python/hello.py
@@ -1,10 +1,9 @@
 ﻿print("hello world")
 import pyodbc 
-cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};Server=59.111.61.113,38065;Database=ds;UID=sa;PWD=Love2016;")
-
+cnxn = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server=111.230.213.219,38065;Database=ds;UID=sa;PWD=Love2016;")
 
 cursor = cnxn.cursor()
-cursor.execute('SELECT top 10 ActorName FROM GameActor')
+cursor.execute('SELECT top 10 ActorName FROM dbo.GameActor where UserID<>1')
 
 for row in cursor:
     print('row = %r' % (row,))
