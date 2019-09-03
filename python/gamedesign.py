d88242343b36326874cb32f1416605f0f010ddad
cpzhang <297191409@qq.com>
2019-09-03 22:25:59 +0800

    pandas

--- python/gamedesign.py
+++
@@ -1,9 +0,0 @@
-﻿print("hello world")
-#import pyodbc 
-#cnxn = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server=59.111.61.113,38065;Database=ds;UID=sa;PWD=Love2016;")
-
-#cursor = cnxn.cursor()
-#cursor.execute('SELECT top 10 ActorName FROM dbo.GameActor where UserID=1')
-
-#for row in cursor:
-#    print('row = %r' % (row,))
