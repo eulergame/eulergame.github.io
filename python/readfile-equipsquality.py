4818d6a4e5db65b47b571dda5be67978852a1192
happycoding2019 <297191409@qq.com>
2019-09-07 15:33:13 +0800

    equip quality

---
+++ python/readfile-equipsquality.py
@@ -0,0 +1,18 @@
+﻿import pyodbc 
+import pandas
+import sqlalchemy
+import matplotlib
+from game import Game
+
+class EquipQuality:
+    engine = sqlalchemy.create_engine(Game.DB)
+    csvFile = "EquipQuality"
+    def __init__(self):
+        self.df = Game.ReadGameCSV(Game.folder + self.csvFile + ".csv",'EquipQuality',Game.gameid)    
+
+    def SaveDB(self):
+        self.df.to_sql(self.csvFile, self.engine,if_exists='replace')
+
+    
+instance = EquipQuality()
+instance.SaveDB()    
