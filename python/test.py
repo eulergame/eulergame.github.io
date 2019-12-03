398f88131842ee49d0b430463a25f385a18e75e1
cp <2971917409@qq.com>
2019-12-03 12:21:54 +0800

    equipment

---
+++ python/test.py
@@ -0,0 +1,20 @@
+﻿#to support encodings
+import io
+
+import os    
+from chardet import detect
+
+# get file encoding type
+def get_encoding_type(file):
+    with open(file, 'rb') as f:
+        rawdata = f.read()
+    return detect(rawdata)['encoding']
+
+#read input file
+with io.open("d:\DeerDevLYFX\Cluster\Agent\AgentServer\AgentServer.cpp", 'r', encoding="cp1252") as file:
+  lines = file.read()
+
+print(lines)
+#write output file
+with io.open("d:\DeerDevLYFX\Cluster\Agent\AgentServer\AgentServer.cpp", 'w', encoding="utf8") as file:
+  file.write(lines.encode('utf-8'))
